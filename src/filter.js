5e1295a5e125129cdba45121d7d21cad8ef1e76e
sidemmmin <sidemmmin@gmail.com>
2024-01-01 18:14:19 +0900

    fix: 날짜 filter filter.js 로 모으고, day-js 라이브러리 설치해서 적용

--- src/filter.js
+++ src/filter.js
@@ -1,10 +1,24 @@
-export function formatDate(value) {
-    const date = value;
-    let month = date.getMonth() + 1;
-    month = month > 9 ? month : `0${month}`;
-    const day = date.getDate();
-    let hours = date.getHours();
-    hours = hours > 9 ? hours : `0${hours}`;
-    const minutes = date.getMinutes();
-    return `${month}-${day} ${hours}:${minutes}`;
+import dayjs from "dayjs";
+
+const formatMMDDHHmm = (value) => {
+    return `${formatDate(value)} ${formatTime(value)}`;
+}
+
+const formatYear = (value) => {
+    return dayjs(value).format('YYYY')
+}
+
+const formatDate = (value) => {
+    return dayjs(value).format('MM-DD');
+}
+
+const formatTime = (value) => {
+    return dayjs(value).format('HH:mm') ;
+}
+
+export default {
+    formatMMDDHHmm,
+    formatYear,
+    formatDate,
+    formatTime
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import axios from 'axios'
+import filter from './filter'
 loadFonts()
 
 const app = createApp(App)
@@ -15,39 +16,6 @@ app.use(vuetify)
 app.use(axios)
 
 app.config.globalProperties.$axios = axios;
-app.config.globalProperties.$filter = {
-    formatYear(value) {
-        var date = new Date(value);
-        var year = date.getFullYear();
-
-        return year
-    },
-    formatDate(value) {
-        var date = new Date(value);
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-
-        if (month < 10)
-            month = '0' + month
-
-        if (day < 10)
-            day = '0' + day
-
-        return month + '-' + day;
-    },
-    formatTime(value) {
-        var date = new Date(value);
-        var hour = date.getHours();
-        var min = date.getMinutes();
-
-        if (hour < 10)
-            hour = '0' + hour
-
-        if (min < 10)
-            min = '0' + min
-
-        return hour + ":" + min ;
-    }
-}
+app.config.globalProperties.$filter = filter;
 
 app.mount('#app')
